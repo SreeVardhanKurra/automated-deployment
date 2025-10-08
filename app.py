0946858dfb77adab56f60c6bd05165b8e3d0ecb4
SreeVardhanKurra <sreevardhankurra@gmail.com>
2025-10-08 21:17:59 +0530

    Update app message for CI/CD test

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, CI/CD Pipeline is working!"
+    return "Hello, CI/CD Pipeline is still working!"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
+
